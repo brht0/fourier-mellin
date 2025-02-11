504e5fe53012f6eac458c806cdcfa81955f97582
brht0 <henrik.toikka@aalto.fi>
2025-02-11 08:31:22 +0200

    dockerfiles compile opencv 4.5.2. x86_64 tested with building wheels

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # manylinux-x86_64
-docker build -f Dockerfile.manylinux_x86_64 -t ghcr.io/htoik/fourier-mellin/manylinux_x86_64:latest .
+# docker build -f Dockerfile.manylinux_x86_64 -t ghcr.io/htoik/fourier-mellin/manylinux_x86_64:latest --load .
+
 # manylinux-aarch64
-docker buildx build --platform linux/arm64 -f Dockerfile.manylinux_aarch64 -t ghcr.io/htoik/fourier-mellin/manylinux_aarch64:latest .
+docker buildx build --platform linux/arm64 -f Dockerfile.manylinux_aarch64 -t ghcr.io/htoik/fourier-mellin/manylinux_aarch64:latest --load .
+# docker buildx build --platform linux/arm64 -f Dockerfile.manylinux_aarch64 -t ghcr.io/htoik/fourier-mellin/manylinux_aarch64:latest --load --progress=plain --no-cache .
--- docker/push_docker.sh
+++ docker/push_docker.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 docker push ghcr.io/htoik/fourier-mellin/manylinux_x86_64:latest
-docker push ghcr.io/htoik/fourier-mellin/manylinux_aarch64:latest
-
+# docker push ghcr.io/htoik/fourier-mellin/manylinux_aarch64:latest

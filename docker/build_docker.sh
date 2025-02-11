a138de4d8ff77c1030bb762e0dd60d6c943ebb73
brht0 <henrik.toikka@aalto.fi>
2025-02-11 10:26:01 +0200

    small changes to scripts, gitignore, updated version to pyproject.toml aswell

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 # manylinux-x86_64
-# docker build -f Dockerfile.manylinux_x86_64 -t ghcr.io/htoik/fourier-mellin/manylinux_x86_64:latest --load .
-
+docker build -f Dockerfile.manylinux_x86_64 -t ghcr.io/htoik/fourier-mellin/manylinux_x86_64:latest --load .
 # manylinux-aarch64
 docker buildx build --platform linux/arm64 -f Dockerfile.manylinux_aarch64 -t ghcr.io/htoik/fourier-mellin/manylinux_aarch64:latest --load .
-# docker buildx build --platform linux/arm64 -f Dockerfile.manylinux_aarch64 -t ghcr.io/htoik/fourier-mellin/manylinux_aarch64:latest --load --progress=plain --no-cache .
